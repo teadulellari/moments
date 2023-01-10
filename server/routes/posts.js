5784f92497fde704cbb5f5d632e282c339982d56
teadulellari <teadulellari@gmail.com>
2023-01-10 20:01:32 +0200

    stylin the components

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
+
+import {getPosts, createPost} from '../controllers/posts.js'
 const router = express.Router();
 
 //http://localhost:5000/posts
 
-router.get('/', (req, res) => {
-res.send('This works!');
-});
+router.get('/', getPosts);
+
+router.post('/', createPost);
 
 
 
