4a735524bb2ce03e5e72c0ae02937cb45e46e0fc
teadulellari <teadulellari@gmail.com>
2023-01-22 21:04:13 +0200

    added Like and Delete functionalities

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,21 +1,28 @@
 import React from "react";
-
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from './styles'; 
 
-const  Posts = () =>{
+const  Posts = ({ setCurrentId }) =>{
+    //useSelector takes a callback function that receives the whole state of the store and must return the specific piece of state that the component needs. In this case, the callback function is (state) => state.posts, which returns the posts property from the state object.
+
+
     const posts = useSelector((state) => state.posts)
     const classes = useStyles();
 
-    console.log(posts);
+
     
     return (
-        <>
-        <h1 className={classes.mainContainer}>POSTS</h1>
-        <Post />
-        <Post />
-        </>
+       !posts.length ? <CircularProgress /> : (
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+            {posts.map((post) => (
+                <Grid key={post._id} item xs={12} sm={6}> 
+                  <Post post={post}  setCurrentId={setCurrentId}/>
+                </Grid>
+            ))}
+        </Grid>
+       )
       
     )
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 
-import {getPosts, createPost} from '../controllers/posts.js'
+import {getPosts, createPost, updatePost, deletePost, likePost} from '../controllers/posts.js'
 const router = express.Router();
 
 //http://localhost:5000/posts
@@ -10,6 +10,11 @@ router.get('/', getPosts);
 
 router.post('/', createPost);
 
+router.patch('/:id', updatePost)
+
+router.delete('/:id', deletePost)
+
+router.patch('/:id/likePost', likePost);
 
 
-export default router;
+export default router
