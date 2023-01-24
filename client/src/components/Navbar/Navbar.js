41447a84eab48da659bf1836829e48a326747486
teadulellari <teadulellari@gmail.com>
2023-01-24 22:38:18 +0200

    Established Google Auth

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,54 +1,78 @@
 import React from "react";
-import { AppBar, Avatar, Button, Toolbar, Typography} from '@material-ui/core'
-import useStyles from './styles';
-import moments from '../../images/moments.png';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import * as actionType from '../../constants/actionTypes';
-
-
-
-
+import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
+import useStyles from "./styles";
+import moments from "../../images/moments.png";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import * as actionType from "../../constants/actionTypes";
 
 const Navbar = () => {
-
   const classes = useStyles();
-//  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
-  const user = useSelector(state => state.auth.authData);
-   console.log(user);
-   const dispatch = useDispatch();
-   const navigate = useNavigate();
+  //  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const user = useSelector((state) => state.auth.authData);
+  console.log(user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-
-
-   const logout = () => {
+  const logout = () => {
     dispatch({ type: actionType.LOGOUT });
-    navigate('/auth');
+    navigate("/auth");
   };
- 
-    return(
-  <AppBar className={classes.appBar} position="static" color="inherit">
-    <div className={classes.brandContainer}>
-    <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">
-      Moments
-    </Typography>
-    <img className={classes.image} src={moments} alt="moments" height="60" />
-    </div>
-    <Toolbar className={classes.toolbar}>
-    {user ? (
+
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography
+          component={Link}
+          to="/"
+          className={classes.heading}
+          variant="h2"
+          align="center"
+        >
+          Moments
+        </Typography>
+        <img
+          className={classes.image}
+          src={moments}
+          alt="moments"
+          height="60"
+        />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
           <div className={classes.profile}>
-            <Avatar className={classes.purple} alt={user.name} src={user.picture}>{user.name?.charAt(0)}</Avatar>
-            <Typography className={classes.userName} variant="h6">{user.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+            <Avatar
+              className={classes.purple}
+              alt={user.name}
+              src={user.picture}
+            >
+              {user.name?.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="secondary"
+              onClick={logout}
+            >
+              Logout
+            </Button>
           </div>
         ) : (
-          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
         )}
-    </Toolbar>
-
-  </AppBar>
-    )
-}
-
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,22 +1,22 @@
-import { AUTH, LOGOUT } from  '../constants/actionTypes';
-import jwt_decode from 'jwt-decode';
-import { googleLogout } from '@react-oauth/google';
+import { AUTH, LOGOUT } from "../constants/actionTypes";
+import jwt_decode from "jwt-decode";
+import { googleLogout } from "@react-oauth/google";
 
-const authReducer = (state= {authData: null}, action) => {
-    switch (action.type) {
-        case AUTH:
-            console.log(action?.data);
-            const userData = jwt_decode(action?.data?.credential)
-            console.log(userData)
-            localStorage.setItem('profile', JSON.stringify({ ...userData }))
-        return { ...state, authData: userData };
-        case LOGOUT:
-            localStorage.clear();
-            googleLogout();
-            return {...state, authData: null};
-     default:
-        return state;       
-    }
-}
+const authReducer = (state = { authData: null }, action) => {
+  switch (action.type) {
+    case AUTH:
+      console.log(action?.data);
+      const userData = jwt_decode(action?.data?.credential);
+      console.log(userData);
+      localStorage.setItem("profile", JSON.stringify({ ...userData }));
+      return { ...state, authData: userData };
+    case LOGOUT:
+      localStorage.clear();
+      googleLogout();
+      return { ...state, authData: null };
+    default:
+      return state;
+  }
+};
 
 export default authReducer;
